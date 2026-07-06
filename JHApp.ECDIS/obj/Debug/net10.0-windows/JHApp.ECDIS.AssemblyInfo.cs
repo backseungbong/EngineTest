28c9897f8ef4efdf5e2b8e54cc2a00271c4e5c86
backseungbong <bsb2527@naver.com>
2026-07-06 16:20:50 +0900

    수정

--- JHApp.ECDIS/obj/Debug/net10.0-windows/JHApp.ECDIS.AssemblyInfo.cs
+++ JHApp.ECDIS/obj/Debug/net10.0-windows/JHApp.ECDIS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JHApp.ECDIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df40970b552c7268cfbef2975efd14ea22d2cc45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae37724ab700777aefd6d59c0a0d8f7c85c5a30c")]
 [assembly: System.Reflection.AssemblyProductAttribute("JHApp.ECDIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JHApp.ECDIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
